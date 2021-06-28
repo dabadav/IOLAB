cf7a448d6d6783f6b0b5ace8fe5004cab2b69346
dantekali <dante.avino@alum.esci.upf.edu>
2021-06-28 18:51:30 +0200

    par

--- iolab-files/parFastqParser.py
+++ iolab-files/parFastqParser.py
@@ -12,25 +12,43 @@ fd = os.open("SRR000049.fastq",os.O_RDONLY)
 p2ch = os.pipe()
 ch2p = os.pipe()
 
-# Parent creates child 'pq' process
+# Parent creates child process
 pid = os.fork()
 
 if (pid == 0):  # Child
+    # Waits reading from the pipep2ch.  
+    # When the parent sends the data, child finds out the line worst quality and
+    # returns to its parent a pair(base, quality) via pipech2pand writes the pair to an ASCII text file calledworst.txt.  
+    # This file iscreated by the child process at the beginning with permissions: rw-rw-r--.
+
     # Infinite loop
     while True:
         c = os.read(p2ch[0],1) # reads from the pipe ch2p
         # Compute worst
-        io.showWorstQlty(filename)
+        io.worstQlty(seqline, qltyline)
         # Worst to txt
         os.write(ch2p[1],c)    # writes to the pipe ch2p
 
 else:
     # Loop. Each iteration, reads the length of a line, a line of sequences and and a line of qualities from the fastq file (passed as an argument 1)
     # Parent sends this information to its child, via pipep2ch
-    for i in range(len):
+    while True:
+        headerline = io.readLine(fd)
+        if headerline:
+            seqline   = io.readLine(fd)
+            breakline = io.readLine(fd)
+            qltyline  = io.readLine(fd)
+            seqlength = io.readLength(headerline)
+        else:
+            break
+
         os.write(p2ch[1],c)    # writes to the pipe p2ch
         c = os.read(ch2p[0],1) # reads from the pipe ch2p
-        read fastq
-    os.write(1,c) # and writesto the stdout (terminal)
+        io.readLine(fd)
+
+    # waits for its child returns the worst pair (base,quality),viach2p.  
+    # When there are no more lines to read,  the parent process print the worst pair to thestandard output, kills its child, clean up and ends
+    
+    os.write(1,c) # and writes to the stdout (terminal)
     os.wait()
 
